71ae6dd5658c446634368ec62f51be74a15ba928
Yeyson Leiva <yeysonlr@gmail.com>
2026-04-23 17:10:34 -0500

    Python

---
+++ Python/intro.py
@@ -0,0 +1,28 @@
+# Fundamentos Basicos Python
+
+# Comentario en una sola linea usar #
+# Comentario Multilinea """ """
+"""
+Este es un comentario multilinea
+"""
+
+# Declarar variables
+"""
+nombre_identifica valor
+
+edad_persona
+edad = inst
+mensaje = 
+
+"""
+
+# Tipo de dato
+
+edad = 18
+print(edad)
+
+mensaje = "Bienvenido a UPN"
+print(mensaje)
+
+# Presentacion mas avanzada uso f" "
+print(f"Usted tiene {edad} años. {mensaje}")
