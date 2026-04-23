e7446354f55e3196c20774b181986260024031e3
Yeyson Leiva <yeysonlr@gmail.com>
2026-04-23 17:19:26 -0500

    Subiendo ejercicio 1 python

---
+++ Python/1_Hallar_Area_Triangulo.py
@@ -0,0 +1,16 @@
+"""
+Algoritmo que Calcula el Area del triangulo
+Leyendo su base y altura, considerando el tipo de dato
+"""
+# Definir Variable y asignar valores
+
+base= 7
+altura= 15
+area = (7*15)/2
+print(f"El area del Triangulo es {float(area)} m2")
+
+# Definir variable, tipo de datos y ingreso de datos por consola
+base = int (input ("Ingrese la base del Triangulo: "))
+altura = int (input ("Ingrese la base del Triangulo: "))
+area = (base*altura)/2
+print(f"El area del Triangulo es {float(area)} m2")
