2e399c14692e91f3f52b9be7ff285429c9c1f71d
Yeyson Leiva <yeysonlr@gmail.com>
2026-05-21 16:49:03 -0500

    Agregando ejecucion

--- CSharp/Ejercicio 1/Program.cs
+++ CSharp/Ejercicio 1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,41 @@ namespace Ejercicio_1
     {
         static void Main(string[] args)
         {
+            //Fecha y Hora
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Leiva Romero, Yeyson");//Programador
+            Console.WriteLine("Ejericio 1 - Edad Persona");
+
+            //Declarar Variable
+            int edad;
+
+            //Entrada de datos
+            //Mostrar mensaje al usuario para ingresar datos
+            Console.WriteLine("Ingrese su edad: ");
+            //Primera forma de guardar el valor ingresado por el usuario
+            //Usando int.Parse
+            edad = int.Parse(Console.ReadLine());
+
+            //Decision
+            /* Si -->if edad >= 18 Entonces {
+             *      Mostrar "Mayor de Edad"
+             *   }
+             * sino else {
+             *      Mostrar "Menor de Edad"
+             *      }
+             * finsi 
+             */
+            if (edad >= 18)
+            {
+                Console.WriteLine("Eres mayor de Edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres menor de Edad");
+            }
+
+            //Detener pantalla
+            Console.ReadKey();
         }
     }
 }
