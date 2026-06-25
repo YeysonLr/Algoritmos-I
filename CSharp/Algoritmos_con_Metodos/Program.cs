d0cb2328f5fbb7d0a83d86a2ff0562e8bbabfdbd
Yeyson Leiva <yeysonlr@gmail.com>
2026-06-25 16:30:37 -0500

    Ejecicios

---
+++ CSharp/Algoritmos_con_Metodos/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritmos_con_Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
